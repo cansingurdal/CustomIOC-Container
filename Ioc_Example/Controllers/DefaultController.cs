e620fed15b2ae5be4e8f26553b59eaa79edc91ab
CANSIN <cansingurdal@hotmail.com>
2018-08-09 11:11:18 +0300

    IOC layer created

---
+++ Ioc_Example/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Ioc_Example.Models;
+
+namespace Ioc_Example.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public void Get()
+        {
+            BildirimManager bildirim = BildirimManagerCreator.Create();
+            bildirim.BildirimYap();
+        }
+    }
+}
---
+++ Ioc_Example/Models/Bildirim.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Ioc_Example.Models
+{
+    public class BildirimManagerCreator
+    {
+        public static BildirimManager Create()
+        {
+            return new BildirimManager(new MesajGonder());
+        }
+    }
+    public class BildirimManager
+    {
+        private IBildirim _bildirim;
+        public BildirimManager(IBildirim bildirim)
+        {
+            this._bildirim = bildirim;
+        }
+
+        public void BildirimYap()
+        {
+            _bildirim.Gonder();
+        }
+    }
+
+    public interface IBildirim
+    {
+        void Gonder();
+    }
+    public class EPostaGonder: IBildirim
+    {
+        public void Gonder()
+        {
+            Debug.WriteLine("EPosta Gonderildi");
+        }
+    }
+    public class MesajGonder: IBildirim
+    {
+        public void Gonder()
+        {
+            Debug.WriteLine("Mesaj Gonderildi");
+        }
+    }
+}
